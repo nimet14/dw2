0c6783e878e4d3cb33234037d62a84b8fc7fe5d9
nimet14 <113248829+nimet14@users.noreply.github.com>
2024-05-20 21:53:44 +0200

    script

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,6 +1,9 @@
 
 docker build -t demo-site:latest .
 
+docker stop demo-container
+docker rm demo-container
+
 docker run -d -t --name demo-container -p 81:80 demo-site:latest
 
 docker exec -i -t demo-container /bin/bash
